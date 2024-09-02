dee487566dfe49582116a8ad9debff7186e60c51
Ken <kennethwarui35@gmail.com>
2024-09-02 21:51:02 +0300

    implemented solution

--- pic/pic.go
+++ pic/pic.go
@@ -2,10 +2,23 @@ package main
 
 import "code.google.com/p/go-tour/pic"
 
-func pic(dx, dy int) [][]unit8 {
-    
-}
+func picc(dx, dy int) [][]uint8 {
+    // slicing using make() funtion
+    pic := make([][]uint8, dy)
+
+    for i := 0; i < dy; i++ {
+        // making sclice of lenght dx
+        pic[i] = make([]uint8, dx)
+    }
+    for y := range pic {
+        for x := range pic[y]{
+            pic[y][x] = uint8(x^y)
+        }
+    }
+    return pic
+}   
+
 
 func main(){
-    pic.Show(pic)
+    picc.Show(picc)
 }
